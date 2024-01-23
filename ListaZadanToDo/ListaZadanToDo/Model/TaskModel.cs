fd35945f531e5c2cf97657ddac61b758a0926791
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-01-23 14:07:21 +0100

    Add task model class

---
+++ ListaZadanToDo/ListaZadanToDo/Model/TaskModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaZadanToDo.Model
+{
+   public class TaskModel
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Importance { get; set; }
+        public string Image
+        {
+            get
+            {
+                if (Importance == "Ważne")
+                    return "https://cdn-icons-png.flaticon.com/512/5619/5619095.png";
+                return "https://upload.wikimedia.org/wikipedia/commons/4/43/Minimalist_info_Icon.png";
+            }
+        }
+    }
+}
