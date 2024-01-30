c2112915f0b0440f57a8d2f8d1ec6839f59a43da
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-01-30 12:38:17 +0100

    Add AddOrEditPage for adding and editing tasks in list and it's xaml content

---
+++ ListaZadanToDo/ListaZadanToDo/AddOrEditPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListaZadanToDo
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddOrEditPage : ContentPage
+    {
+        public AddOrEditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- ListaZadanToDo/ListaZadanToDo/MainPage.xaml.cs
+++ ListaZadanToDo/ListaZadanToDo/MainPage.xaml.cs
@@ -33,7 +33,7 @@ namespace ListaZadanToDo
                 tasks = JsonConnection.GetFromFile();
                 TasksList.ItemsSource = tasks;
             }
-        }
+        }        
 
     }
 }
